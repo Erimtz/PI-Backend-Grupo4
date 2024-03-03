a09f52e26e90784ec48828185533086e5d7cb636
aleandreslg <aleandreslg@gmail.com>
2024-03-03 20:29:05 -0300

    modularizacion del metodo createPurchase

--- src/main/java/com/gym/services/PurchaseServiceImpl.java
+++ src/main/java/com/gym/services/PurchaseServiceImpl.java
@@ -41,12 +41,45 @@ public class PurchaseServiceImpl implements PurchaseService{
 
 
     public PurchaseResponseDTO createPurchase(PurchaseRequestDTO requestDTO, String token) {
+        // Obtener la cuenta del usuario a partir del token
+        Account account = getAccountFromToken(token);
 
+        // Construir la compra con los detalles proporcionados en la solicitud
+        Purchase purchase = buildPurchase(requestDTO, account);
+
+        // Agregar los cupones a la compra
+        addCouponsToPurchase(requestDTO, purchase);
+
+        // Calcular totales y descuentos, y guardar la compra
+        calculateAndSavePurchaseTotals(purchase);
+
+        // Construir y devolver la respuesta de la compra
+        return buildPurchaseResponse(purchase);
+    }
+
+    private Double calculateSubtotal(PurchaseDetail purchaseDetail) {
+        if (purchaseDetail.getProduct() != null && purchaseDetail.getProduct().getPrice() != null) {
+            return purchaseDetail.getProduct().getPrice().doubleValue() * purchaseDetail.getQuantity();
+        } else {
+            // Manejo del caso en que el precio del producto es null
+            throw new IllegalArgumentException("Price of the product is null");
+        }
+    }
+
+//    private Double calculateTotalAfterDiscounts(Double total, List<Coupon> couponsApplied, StoreSubscription storeSubscription) {
+//        Double totalAfterCoupons = total - couponsApplied.stream().mapToDouble(Coupon::getAmount).sum();
+//        return totalAfterCoupons - storeSubscription.getPrice();
+//    }
+
+    private Account getAccountFromToken(String token) {
         Account account = accountService.getAccountFromToken(token);
         if (account == null) {
             throw new IllegalArgumentException("No se pudo obtener la cuenta del usuario");
         }
+        return account;
+    }
 
+    private Purchase buildPurchase(PurchaseRequestDTO requestDTO, Account account) {
         Purchase purchase = new Purchase();
         purchase.setPurchaseDate(LocalDate.now());
         purchase.setAccount(account);
@@ -69,10 +102,8 @@ public class PurchaseServiceImpl implements PurchaseService{
                 Product product = productRepository.findById(detailDTO.getProductId())
                         .orElseThrow(() -> new IllegalArgumentException("Product not found with ID: " + detailDTO.getProductId()));
 
-                // Verificar si hay suficiente stock para este producto
                 Product updatedProduct = productService.updateStockPurchase(product.getId(), Long.valueOf(detailDTO.getQuantity()));
 
-                // Si hay suficiente stock, crear el detalle de compra
                 PurchaseDetail detail = new PurchaseDetail();
                 detail.setProduct(updatedProduct);
                 detail.setQuantity(detailDTO.getQuantity());
@@ -80,48 +111,47 @@ public class PurchaseServiceImpl implements PurchaseService{
             }
             purchase.setPurchaseDetails(purchaseDetails);
         }
+        return purchase;
+    }
 
-        // Obtener los IDs de los cupones aplicados y agregarlos a la compra
-        List<Long> couponIds = requestDTO.getCouponsIds();
-        if (couponIds != null && !couponIds.isEmpty()) {
-            List<Coupon> appliedCoupons = new ArrayList<>();
-            for (Long couponId : couponIds) {
-                // Buscar y convertir el cupón dentro del mismo bloque try-catch
-                try {
-                    // Buscar el cupón por su ID
-                    ResponseCouponDTO responseCouponDTO = couponService.getCouponById(couponId);
+    private void calculateAndSavePurchaseTotals(Purchase purchase) {
 
-                    // Verificar si el cupón fue encontrado
-                    if (responseCouponDTO != null) {
-                        // Convertir el DTO del cupón a la entidad del cupón
-                        Coupon coupon = new Coupon();
-                        coupon.setId(responseCouponDTO.getId());
-                        coupon.setIssueDate(responseCouponDTO.getIssueDate());
-                        coupon.setDueDate(responseCouponDTO.getDueDate());
-                        coupon.setAmount(responseCouponDTO.getAmount());
-                        coupon.setSpent(responseCouponDTO.getSpent());
+        Double total = calculateTotal(purchase);
+        Double discount = calculateDiscount(purchase);
+        Double totalAfterDiscounts = total - discount;
 
-                        // Agregar el cupón convertido a la lista de cupones aplicados
-                        appliedCoupons.add(coupon);
-                    } else {
-                        // Si el cupón no fue encontrado, lanzar una excepción
-                        logger.error("No se encontró ningún cupón con ID: {}", couponId);
-                        throw new IllegalArgumentException("Coupon not found with ID: " + couponId);
-                    }
-                } catch (IllegalArgumentException e) {
-                    // Capturar y relanzar la excepción si ocurre un error al buscar el cupón
-                    logger.error("Error al buscar el cupón con ID: {}", couponId, e);
-                    throw new IllegalArgumentException("Error al buscar el cupón con ID: " + couponId);
-                }
-            }
-            // Establecer la lista de cupones aplicados en la compra
-            purchase.setCouponsApplied(appliedCoupons);
+        purchase = purchaseRepository.save(purchase);
+    }
+
+    private Double calculateTotal(Purchase purchase) {
+        Double total = 0.0;
+
+        if (purchase.getPurchaseDetails() != null) {
+            total += purchase.getPurchaseDetails().stream()
+                    .mapToDouble(detail -> detail.getProduct().getPrice() * detail.getQuantity())
+                    .sum();
         }
 
-        // Guardar la compra luego de aplicar los cupones
-        purchase = purchaseRepository.save(purchase);
+        if (purchase.getStoreSubscription() != null) {
+            total += purchase.getStoreSubscription().getPrice();
+        }
 
-        // Realizar otros cálculos y obtener la respuesta
+        return Math.round(total * 100.0) / 100.0;
+    }
+
+    private Double calculateDiscount(Purchase purchase) {
+        Double discount = 0.0;
+
+        if (purchase.getCouponsApplied() != null) {
+            discount += purchase.getCouponsApplied().stream()
+                    .mapToDouble(Coupon::getAmount)
+                    .sum();
+        }
+
+        return discount;
+    }
+
+    private PurchaseResponseDTO buildPurchaseResponse(Purchase purchase) {
         List<PurchaseDetailResponseDTO> detailDTOs = new ArrayList<>();
         if (purchase.getPurchaseDetails() != null) {
             detailDTOs = purchase.getPurchaseDetails().stream()
@@ -135,23 +165,13 @@ public class PurchaseServiceImpl implements PurchaseService{
                     .collect(Collectors.toList());
         }
 
-        // Calcular totales y descuentos
-        Double total = (detailDTOs.stream().mapToDouble(PurchaseDetailResponseDTO::getSubtotal).sum()) + purchase.getStoreSubscription().getPrice();
-        total = Math.round(total * 100.0) / 100.0;
+        Double subscriptionPrice = purchase.getStoreSubscription() != null ? purchase.getStoreSubscription().getPrice() : 0;
 
-        Double discount = 0.0;
-        if (purchase.getCouponsApplied() != null) {
-            discount = purchase.getCouponsApplied().stream()
-                    .mapToDouble(Coupon::getAmount)
-                    .sum();
-        }
-
+        Double total = calculateTotal(purchase);
+        Double discount = calculateDiscount(purchase);
         Double totalAfterDiscounts = total - discount;
         totalAfterDiscounts = Math.round(totalAfterDiscounts * 100.0) / 100.0;
 
-        Double subscriptionPrice = purchase.getStoreSubscription() != null ? purchase.getStoreSubscription().getPrice() : 0;
-
-        // Crear la lista de cupones aplicados para la respuesta
         List<CouponResponseDTO> couponsResponseDTO = new ArrayList<>();
         if (purchase.getCouponsApplied() != null) {
             couponsResponseDTO = purchase.getCouponsApplied().stream()
@@ -162,17 +182,33 @@ public class PurchaseServiceImpl implements PurchaseService{
         return new PurchaseResponseDTO(detailDTOs, subscriptionPrice, total, couponsResponseDTO, discount, totalAfterDiscounts);
     }
 
-    private Double calculateSubtotal(PurchaseDetail purchaseDetail) {
-        if (purchaseDetail.getProduct() != null && purchaseDetail.getProduct().getPrice() != null) {
-            return purchaseDetail.getProduct().getPrice().doubleValue() * purchaseDetail.getQuantity();
-        } else {
-            // Manejo del caso en que el precio del producto es null
-            throw new IllegalArgumentException("Price of the product is null");
-        }
-    }
+    private void addCouponsToPurchase(PurchaseRequestDTO requestDTO, Purchase purchase) {
+        List<Long> couponIds = requestDTO.getCouponsIds();
+        if (couponIds != null && !couponIds.isEmpty()) {
+            List<Coupon> appliedCoupons = new ArrayList<>();
+            for (Long couponId : couponIds) {
+                try {
+                    ResponseCouponDTO responseCouponDTO = couponService.getCouponById(couponId);
+                    if (responseCouponDTO != null) {
 
-    private Double calculateTotalAfterDiscounts(Double total, List<Coupon> couponsApplied, StoreSubscription storeSubscription) {
-        Double totalAfterCoupons = total - couponsApplied.stream().mapToDouble(Coupon::getAmount).sum();
-        return totalAfterCoupons - storeSubscription.getPrice();
+                        Coupon coupon = new Coupon();
+                        coupon.setId(responseCouponDTO.getId());
+                        coupon.setIssueDate(responseCouponDTO.getIssueDate());
+                        coupon.setDueDate(responseCouponDTO.getDueDate());
+                        coupon.setAmount(responseCouponDTO.getAmount());
+                        coupon.setSpent(responseCouponDTO.getSpent());
+
+                        appliedCoupons.add(coupon);
+                    } else {
+                        logger.error("No se encontró ningún cupón con ID: {}", couponId);
+                        throw new IllegalArgumentException("Coupon not found with ID: " + couponId);
+                    }
+                } catch (IllegalArgumentException e) {
+                    logger.error("Error al buscar el cupón con ID: {}", couponId, e);
+                    throw new IllegalArgumentException("Error al buscar el cupón con ID: " + couponId);
+                }
+            }
+            purchase.setCouponsApplied(appliedCoupons);
+        }
     }
 }
